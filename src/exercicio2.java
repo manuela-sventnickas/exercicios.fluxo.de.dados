26bc7180a6fc2fec2658132e58fc832baa9b76ca
manue <manu.svent@gmail.com>
2023-08-09 16:15:43 -0300

    Initial commit

---
+++ src/exercicio2.java
@@ -0,0 +1,9 @@
+public class exercicio2 {
+    public static void main(String[] args) {
+        int x = 10;
+        while (x <= 20) {
+            System.out.println("item " + x);
+            x++;
+        }
+    }
+}
