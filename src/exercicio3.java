1871d5a7efd2d4c05813352310ec101ea9ec1c7f
manue <manu.svent@gmail.com>
2023-08-09 16:18:29 -0300

    exercicio 3

---
+++ src/exercicio3.java
@@ -0,0 +1,9 @@
+public class exercicio3 {
+    public static void main(String[] args) {
+        int y = 10;
+        do {
+            System.out.println("item " + y);
+            y++;
+        } while (y <= 20);
+    }
+}
